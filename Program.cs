207d2d9f63c14ec13b357c17cb34b3dd59c26898
JongD <JongD@JONGDO>
2024-05-15 13:12:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_2._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TipCalculator();
+        }
+        static void TipCalculator()
+        { 
+            Console.WriteLine("Welcome to tip calculator.");
+            Console.WriteLine();
+            Console.WriteLine("Please enter your bill total: ");
+            decimal totalBill = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter the wanted tip percentage: (e.g) 10% 15% 20%. ");
+            decimal tipPercentage = Convert.ToDecimal(Console.ReadLine());
+            decimal totalPrice = (tipPercentage/100 * totalBill + totalBill);
+            Console.WriteLine("Your total bill including the tip is: " + totalPrice);
+        }        
+    }
+
+
+
+
+    //Design a tip calculator : enter the bill total, tip % and display grand total after adding tip. 
+}
